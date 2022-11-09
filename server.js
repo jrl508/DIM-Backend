7120ad029c26ac29ffe4a099e671342644a1dd2f
Ricardo Lafontant <Ricky@SaaShas-MacBook-Pro.local>
2022-11-08 22:21:07 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@dev0.8vcj7da.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 3005;
+
+mongoose.connect(CONNECTION_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Running on port : ${PORT}`))
+    })
+    .catch((error) => {
+        console.log(error.message)
+    });
