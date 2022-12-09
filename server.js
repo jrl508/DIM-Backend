ee3cd0cf42cd682ec69cd0d3bac84b0abd3c83eb
Ricky <ricky@SaaShas-MacBook-Pro-3.local>
2022-12-08 20:39:11 -0500

    update projects route

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
 import projectRouter from "./routes/project.routes.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
@@ -14,21 +14,20 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/user', userRouter);
-app.use('/project', projectRouter);
-
+app.use("/user", userRouter);
+app.use("/projects", projectRouter);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@dev0.8vcj7da.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 3005;
 
-mongoose.connect(CONNECTION_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server Running on port : ${PORT}`))
-    })
-    .catch((error) => {
-        console.log(error.message)
-    });
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Running on port : ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
